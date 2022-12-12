fee4ab0f7f3e05e6bb3120640b761d217d19fc49
muhammedsaamy <muhammedsaamy@gmail.com>
2022-12-12 12:39:48 +0200

    ToDo List commit

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Task } from 'src/app/model/task';
+import { CrudService } from 'src/app/service/crud.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  taskObj:Task=new Task();
+  taskArr:Task[]=[];
+  addTaskValue:string='';
+  editTaskValue:string='';
+  // dnTask:string=''
+  constructor(private crudService:CrudService) { }
 
   ngOnInit(): void {
+    this.addTaskValue=''
+    this.editTaskValue=''
+    this.taskObj=new Task();
+    this.taskArr=[]
+    this.getAllTasks()
   }
+
+  getAllTasks(){
+    this.crudService.getAllTask().subscribe((response=>{
+      this.taskArr=response;
+    })
+        //,(err:Error)=>{alert(err.message)}
+    )
+  }
+  addTask(){
+    if(this.addTaskValue==''){
+      alert('you should enter a task')
+    }
+    else{
+    this.taskObj.task_name=this.addTaskValue;
+    this.crudService.addTask(this.taskObj).subscribe(data=>{
+        this.ngOnInit()
+        this.addTaskValue=''
+    }
+
+    //,(err:Error)=>{alert(err.message)}
+)}
+  }
+  editTask(){
+    if(this.editTaskValue==''){
+      alert('update value should not be empty')
+    }
+    else{
+    this.taskObj.task_name=this.editTaskValue
+    this.crudService.editTask(this.taskObj).subscribe((data)=>{
+      this.ngOnInit();
+    }
+    //,(err:Error)=>{alert(err.message)}
+)
+  }
+  }
+
+  deletTask(eTask:Task){
+    try{
+      this.crudService.deletTask(eTask).subscribe((data)=>{
+        this.ngOnInit();
+      }
+  )
+    }
+    catch{
+      alert('faild to add Task')
+    }
+
+  }
+
+
+  call(eTask:Task){
+    this.taskObj=eTask;
+    this.editTaskValue=eTask.task_name;
+  }
+
+  // doneTask(taskName:Task){
+  //   this.dnTask=taskName.task_name+" (Done)"
+  //   // this.doneBtn.nativeElement.value=taskName.task_name + "(Done)"
+  //   console.log('dn')
+  // }
 
 }
--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from '../model/task';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
-
-  constructor() { }
+  serviceUrl:string
+  constructor(private http:HttpClient) {
+    this.serviceUrl='http://localhost:3000/tasks'
+  }
+  addTask(task:Task):Observable<Task>
+  {
+    return this.http.post<Task>(this.serviceUrl,task)
+  }
+  getAllTask():Observable<Task[]>
+  {
+    return this.http.get<Task[]>(this.serviceUrl)
+  }
+  deletTask(task:Task):Observable<Task>
+  {
+    return this.http.delete<Task>(this.serviceUrl+'/'+task.id)
+  }
+  Task(task:Task):Observable<Task>
+  {
+    return this.http.post<Task>(this.serviceUrl,task)
+  }
+  editTask(task:Task):Observable<Task>
+  {
+    return this.http.put<Task>(this.serviceUrl+'/'+task.id,task)
+  }
 }
